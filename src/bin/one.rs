7f0dbb594f2fb20a28148955a1e54d38811d8b6a
Dan Knutson <dan.knutson@gmail.com>
2019-12-01 15:03:47 -0800

    day one

--- src/bin/one.rs
+++ src/bin/one.rs
@@ -1,30 +1,47 @@
 use advent_2019::*;
-use itertools::Itertools;
+use itertools::iterate;
 
 fn main() -> StdResult<()> {
     let input = include_str!("../../data/day_one.txt");
 
-    println!("Solving day one...");
-    let solution = solve(input)?;
-    println!("{}", solution);
+    let parsed: Result<Vec<i32>, _> = input.lines().map(|line| line.parse()).collect();
+    let parsed = parsed?;
+
+    let part_one = solve_part_one(&parsed);
+    println!("part one: {}", part_one);
+
+    let part_two = solve_part_two(&parsed);
+    println!("part two: {}", part_two);
 
     Ok(())
 }
 
-fn solve(input: &str) -> StdResult<String> {
-    let solution = format!("got input: {}", input);
-    let solution = solution
-        .chars()
-        .interleave(vec!['a', 'b', 'c', 'd'])
-        .collect();
+fn solve_part_one(input: &[i32]) -> i32 {
+    input.iter().map(fuel_calc).sum()
+}
 
-    Ok(solution)
+fn solve_part_two(input: &[i32]) -> i32 {
+    input.iter().map(total_fuel).sum()
+}
+
+fn fuel_calc(mass: &i32) -> i32 {
+    mass / 3 - 2
+}
+
+fn total_fuel(mass: &i32) -> i32 {
+    let mass = fuel_calc(mass);
+    iterate(mass, fuel_calc).take_while(|&m| m > 0).sum()
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_total_fuel() {
+        let mass = 100756;
+        let result = total_fuel(&mass);
+
+        assert_eq!(result, 50346);
     }
 }
